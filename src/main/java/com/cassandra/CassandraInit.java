c3983ee336beeabc3df034afc3e18b6d4fa27cf9
Manisha Patlolla <manisha@ninjavan.sg>
2016-10-09 02:37:52 +0800

    cassandra init

--- src/main/java/com/cassandra/CassandraInit.java
+++ src/main/java/com/cassandra/CassandraInit.java
@@ -1,6 +1,7 @@
 package com.cassandra;
 
-import com.cassandra.dump.DumpNewOrderTransaction;
+import com.cassandra.dump.*;
+import com.cassandra.transactions.StockLevelTransaction;
 import com.datastax.driver.core.Session;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -18,13 +19,18 @@ public class CassandraInit {
 
         }
 
-        // dump data
-        // new DumpWarehouse().dump();
-        // new DumpDistrict().dump();
-
-        DumpNewOrderTransaction dump = new DumpNewOrderTransaction();
-//        dump.dumpNewOrderTransactionData();
-        dump.dumNewOrderTransactionInBatches();
+        // dump static tables
+        new DumpWarehouse().dump();
+        new DumpDistrict().dump();
+        new DumpCustomer().dump();
+        new DumpStock().dump();
+        
+        //Dump dynamic tables
+        new DumpCustomerData().dump();
+        new DumpStockLevelTransaction().dump();
+        new DumpNextOrder().dump();
+        new DumpNewOrderTransaction().dump();
+        new DumpOrderStatusTransaction().dump();
 //        new DumpCustomer().dump();
         CassandraSession.closeSession();
         logger.info("Program Ended!");
